a43bd6f6a0b8d90b934d611948d84da66c22781e
Tatiana Naumova <tatiananaumova5@gmail.com>
2023-06-28 12:16:41 -0400

    Add-n in example.spec.js and add @type/jest

--- specs/example.spec.js
+++ specs/example.spec.js
@@ -2,11 +2,19 @@ describe('Operations with numbers', () => {
   const a = 2
   const b = 3
 
-  test('2 + 3 = 5', () => {
+  test('add', () => {
     expect(a + b).toEqual(5)
   })
 
-  test('2 - 3 = -1', () => {
+  test('substract', () => {
     expect(a - b).toEqual(-1)
+  })
+
+  test('multiply', () => {
+    expect(a * b).toEqual(6)
+  })
+
+  test('division', () => {
+    expect(b / a).toEqual(1.5)
   })
 })
