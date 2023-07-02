adb56e9c4b2c500aa07cf6b8ea274fac6227d5d7
Tatiana Naumova <tatiananaumova5@gmail.com>
2023-07-01 23:26:59 -0400

    Add user.spec.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,13 @@
 import 'dotenv/config'
+//import {client} from './common'
+
+// const setup =  async () =>{
+//   const authResponse = await client.post('/auth', {
+//     login: process.env.LOGIN,
+//     password: process.env.PASSWORD,
+//   })
+//
+//   process.env['TOKEN'] = response.data.token
+// }
+
+//await setup()
---
+++ specs/user.spec.js
@@ -0,0 +1,24 @@
+import {client} from 'common'
+
+describe('User', ()=>{
+  beforeall(async () =>{
+    const authResponse = await client.post('/auth', {
+      login: process.env.LOGIN,
+      password: process.env.PASSWORD,
+    })
+
+    process.env['TOKEN'] = response.data.token
+  })
+
+  test('Create user', async ()=>{
+    const token = process.env.token
+    const createUserResponse = await client
+      .post('/users', null, {headers:{'Authorization':`Bearer ${process.env.TOKEN}`}})
+
+    expect(createUserResponse.status).toEqual(200)
+    expect(createUserResponse.data).toEqual({
+      id:expect.any(String),
+      amount: expect.any(Number),
+    })
+    })
+  })
